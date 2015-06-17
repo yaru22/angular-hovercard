3dd65fc5ea2635af3ff8e9e540e4401160d7adbe
Graham Laidler <graham.laidler@accelerata.co.uk>
2015-06-17 16:04:54 +0100

    moved the hover in and hover out callbacks
    Hi, I had trouble using your component when wrapping in a directive of my own - the callbacks were never called.  I modified the declaration of the hover in and out callbacks as optional bound functions on the scope, and all hooked up.

--- dist/angular-hovercard.js
+++ dist/angular-hovercard.js
@@ -4,13 +4,14 @@ angular.module('yaru22.hovercard', ['yaru22.hovercard.tmpls']).directive('hoverc
     restrict: 'E',
     transclude: true,
     templateUrl: 'template/angular-hovercard.tmpl',
-    scope: true,
+    scope: {
+       onHoverIn: '&?',
+       onHoverOut: '&?'
+    },
     link: function ($scope, $element, $attrs) {
       $scope.show = {};
       $scope.show.card = false;
       $scope.hoverTmplUrl = $attrs.hoverTmplUrl;
-      $scope.onHoverIn = $scope.$eval($attrs.onHoverIn);
-      $scope.onHoverOut = $scope.$eval($attrs.onHoverOut);
       var placement = $attrs.placement || 'bottomRight';
       $scope.hoverLabelStyle = {};
       if ($attrs.labelColor) {
